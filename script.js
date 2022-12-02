b058a40066edec91c076b0cf18ef824672450ef0
John Kutch <116672999+jckutch@users.noreply.github.com>
2022-12-02 12:10:26 -0500

    re-wrote header html

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var startBtnEl = document.querySelector("#startGameBtn");
 var timeCounterEl = document.querySelector("#totalTime");
 var questionEl = document.querySelector("#question");
 var questSiteEl = document.querySelector("#questContainer");
+var buttonSiteEl = document.querySelector("#buttonsDiv")
 var highScoreEl = document.querySelector("#highScore");
 var highScoreStoreEl = doument.querySelector("#highScoreStore");
+
+startBtnEl.addEventListener('mouseup', startGame);
 
 // creates timer for game
 var timer = 45
@@ -94,7 +97,7 @@ var questions = [{
     }]
 }]
 
-function showQuestion() {
+function showQuest() {
     var displayQuest = questions[questList];
     h1El.textContent = displayQuest.title;
    questSiteEl.textContent = displayQuest.question;
@@ -108,7 +111,27 @@ for (var i = 0; i < displayQuest.choices.length; i++) {
     choiceBtn.classList.add('buttons')
     choiceBtn.classList.add('whiteText')
     choiceBtn.textContent = displayQuest.choices[i].answer
-    buttonsArea.appendChild(choiceBtn);
+    buttonSiteEl.appendChild(choiceBtn);
 }
 questList++
+}
+
+function startGame() {
+    // if we've already completed the game and there are more than 1 children in the questionsAreaDiv
+    if (questionAreaDiv.childElementCount > 1){
+        // we remove the form
+        questionAreaDiv.removeChild(formHighScore)
+    }
+    // otherwise we remove the start button which we know is there.
+    while (buttonsArea.firstChild){
+        buttonsArea.removeChild(buttonsArea.firstChild)
+    }
+    while (highScoreArea.firstChild){
+        highScoreArea.removeChild(highScoreArea.firstChild)
+    }
+    timer = 45
+    questionList = 0
+    startTimer();
+    showQuest();
+    return
 }
